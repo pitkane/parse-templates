8371e61f647f54c7d419a18f33ff4810a7b8a9fd
pitkane <mikko@mbit.fi>
2017-01-07 16:02:43 +0200

    Doh

--- index.js
+++ index.js
@@ -1,40 +1,127 @@
 let path = require('path');
 let walk = require('walk');
 const fs = require('fs');
-let files   = [];
+let files = [];
+let cheerio = require('cheerio');
 
 // Walker options
-let walker  = walk.walk('/Users/Mikko/vilkas/temp_repos/FI_VILKAS', { followLinks: false });
-
-walker.on('file', function(root, stat, next) {
-    // Add this file to the list of files
-    // console.log(path.extname(stat.name));
-    if(path.extname(stat.name) == '.html') {
-      files.push(root + '/' + stat.name);
-    }
-    next();
+let walker = walk.walk('/Users/Mikko/vilkas/temp_repos/FI_VILKAS', {
+	followLinks: false
 });
 
-walker.on('end', function() {
-    // console.log(files);
-    files.forEach(file => {
-      // console.log(file);
-      // let $ = cheerio.load('<h2 class="title">Hello world</h2>');
-      fs.readFile(file, function () {
-      // doStuff 
-        console.log("moro");
-        next();
-      });
-    });
+// collect all the files for scraping
+walker.on('file', function (root, stat, next) {
+	// Add this file to the list of files
+	// console.log(path.extname(stat.name));
+	if (path.extname(stat.name) == '.html') {
+		files.push(root + '/' + stat.name);
+	}
+	next();
 });
 
-let cheerio = require('cheerio');
-let $ = cheerio.load('<h2 class="title">Hello world</h2>');
+let htmlData = [];
 
-$('h2.title').text('Hello there!');
-$('h2').addClass('welcome');
+// scrape
+walker.on('end', function () {
+	// console.log(files);
+	files.forEach(file => {
 
-$.html();
+		let readFile = fs.readFileSync(file);
+
+		var re = /<script\b[^>]*>([\s\S]*?)<\/script>/gm;
+		var match;
+		while (match = re.exec(readFile)) {
+			// full match is in match[0], whereas captured groups are in ...[1], ...[2], etc.
+			// console.log(match[0]);
+			let json = {
+				filename: file,
+				data: match[1]
+			};
+			json.data = json.data.replace("//<![CDATA[","");
+			json.data = json.data.replace("//]]>","");
+			// console.log(json.data)
+			htmlData.push(json);
+		
+		}
+		// let $ = cheerio.load(fs.readFileSync(file), {
+		// 	// normalizeWhitespace: true,
+		// 	// decodeEntities: true
+		// });
+		// $('script').each(function (i, elem) {
+		// 	console.log(elem);
+		// 	let json = {
+		// 		filename: file,
+		// 		data: $(this).html()
+		// 	};
+		// 	json.data = json.data.replace("//<![CDATA[","");
+		// 	json.data = json.data.replace("//]]>","");
+		// 	// console.log(json.data)
+		// 	htmlData.push(json);
+		// });
+	});
+
+	// console.log(htmlData);
+	let startHTML = fs.readFileSync('./boilerplate_start.html');
+	let endHTML = fs.readFileSync('./boilerplate_end.html');
+	fs.writeFile("./output.html", startHTML, function (err) {
+		if (err) {
+			return console.log(err);
+		}
+	});
+
+	htmlData.forEach(function(element) {
+		let html = "<h2>" + element.filename + "</h2>";
+		html += "<pre><code class='js'>";
+		html += element.data;
+		html += "</code></pre>";
+		// buildHTML('#content').append(html);
+		fs.appendFile('./output.html', html, function (err) {
+			// return console.log(err);
+	    });
+	});
+
+	fs.appendFile('./output.html', endHTML, function (err) {
+			// return console.log(err);
+	    });
+
+	// buildHTML('#content').text('Hello there!');
+	// console.log(buildHTML.html());
+
+	// fs.writeFile("./output.html", buildHTML.html(), function (err) {
+	// 	if (err) {
+	// 		return console.log(err);
+	// 	}
+	// 	console.log("The file output.html was saved!");
+	// });
+
+});
+
+
+
+
+
+			// console.log(elem)
+			// $string = str_replace("//<![CDATA[","",$string);
+			// $string = str_replace("//]]>","",$string);
+			// console.log();
+			// console.log(elem.html());
+
+
+
+
+
+
+
+// console.log(file);
+// let $ = cheerio.load('<h2 class="title">Hello world</h2>');
+// fs.readFile(file, function () {
+// // doStuff 
+//   console.log("moro");
+//   // next();
+// });
+
+
+// $.html();
 //=> <h2 class="title welcome">Hello there!</h2>
 
 
